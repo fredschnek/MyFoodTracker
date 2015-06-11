2ca7433e0fccb0ace14817a628365824c83c405b
Frederico Schnekenberg <fredschnek@Freds-MacBookPro.local>
2015-06-11 10:26:17 -0300

    Initial Commit

---
+++ MyFoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFoodTracker
+//
+//  Created by Frederico Schnekenberg on 11/06/15.
+//  Copyright (c) 2015 Frederico Schnekenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
